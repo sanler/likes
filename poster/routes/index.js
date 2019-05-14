b3298a77342d58b0e45c040333a2ead4b91e7265
sanler <sanler110489@gmail.com>
2019-05-14 12:54:20 +0200

    Contador Like/Unlikes funcionando

---
+++ poster/routes/index.js
@@ -0,0 +1,36 @@
+/*var express = require('express');
+var router = express.Router();
+
+/* GET home page. *//*
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+*/
+
+
+
+
+let router = require('express').Router();
+
+let Post = require('./../models/post');
+
+router.get('/', (req, res, next) => {
+    Post.find().exec((err, posts) => {
+        res.render('index', { posts: posts });
+    });
+
+});
+
+
+
+router.post('/posts/:id/act', (req, res, next) => {
+  const action = req.body.action;
+  const counter = action === 'Like' ? 1 : -1;
+  Post.update({_id: req.params.id}, {$inc: {likes_count: counter}}, {}, (err, numberAffected) => {
+      res.send('');
+  });
+});
+module.exports = router;
